19705ebacd3393162d6c5e30db0fb8f631a88e29
Michael Stewart <tgm@vt.edu>
2015-04-23 10:48:22 +0900

    successfully creating conference

--- app/scripts/controllers/new-conference.js
+++ app/scripts/controllers/new-conference.js
@@ -26,6 +26,25 @@ angular.module('svhubApp')
 
     $scope.submit = function () {
       console.log('new conference', $scope.conference);
+
+      var Conference = Parse.Object.extend("Conference");
+      var newConf = new Conference();
+
+      newConf.set("conf_name", $scope.conference.name);
+      newConf.set("conf_start", $scope.conference.start);
+      newConf.set("conf_end", $scope.conference.end);
+
+      newConf.save(null, {
+       success: function(conference) {
+         // Execute any logic that should take place after the object is saved.
+         alert('New object created with objectId: ' + conference.id);
+       },
+       error: function(conference, error) {
+         // Execute any logic that should take place if the save fails.
+         // error is a Parse.Error with an error code and message.
+         alert('Failed to create new object, with error code: ' + error.message);
+       }
+      });
     };
 
     // var listScope = $scope;
